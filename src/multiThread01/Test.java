4ba426918768a33fda2ec773ff435ab5cfdd7da7
WangZhiHeng <1050589186@qq.com>
2019-07-30 01:48:22 +0800

    测试是否创建分支成功

---
+++ src/multiThread01/Test.java
@@ -0,0 +1,14 @@
+package multiThread01;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Auther: 王志恒
+ * @Date: 2019/7/30 01:47
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试是否创建分支成功");
+    }
+}
