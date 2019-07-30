7ee6078abc9650c29956f7278c242229fa5d6112
WangZhiHeng <1050589186@qq.com>
2019-07-30 22:19:57 +0800

    Revert "添加静态代理模式"
    This reverts commit 99b9607a0a8f15d98dbf34bd7f6a12db4b71caac.

--- src/multiThread01/Test.java
+++ src/multiThread01/Test.java
@@ -1,9 +1,6 @@
 package multiThread01;
 
 import java.sql.SQLOutput;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
 
 /**
  * @Auther: 王志恒
@@ -12,30 +9,6 @@ import java.util.Hashtable;
  */
 public class Test {
     public static void main(String[] args) {
-
-        String s = "aaaaa";
-        System.out.println(s+"bbb".hashCode());
-        System.out.println(s.hashCode());
-        printPth(s);
-
-        System.out.println(">>>>>>>>>>>>>>>>>>>>");
-        StringBuilder sb = new StringBuilder("bbb");
-        System.out.println(sb.hashCode());
-        sbPrint(sb);
-    }
-
-
-   static void  printPth(String s) {
-       System.out.println("-------------------");
-       System.out.println(s.hashCode());
-        s+="2222";
-       System.out.println(s.hashCode());
-   }
-    static void  sbPrint(StringBuilder s) {
-        System.out.println("-------------------");
-        System.out.println(s.hashCode());
-        s.append("222");
-        System.out.println(s.hashCode());
-        System.out.println(s);
+        System.out.println("测试是否创建分支成功");
     }
 }
